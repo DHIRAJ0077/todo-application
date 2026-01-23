04a362e0d10814ede83dc147859cd2566727b8ec
castradeveloper1 <castradeveloper1@gmail.com>
2026-01-23 18:02:00 +0530

    fix-build-

--- backend/models/prisma.js
+++ backend/models/prisma.js
@@ -1,11 +1,5 @@
-import 'dotenv/config'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') {
-  prisma.$on('query', (e) => console.log('Query:', e.query))
-  prisma.$on('error', (e) => console.log('Error:', e.message))
-}
-
-export default prisma
+export default prisma;
